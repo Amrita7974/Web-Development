15babd9a2a3ee5f663f7b6d1c6419da6f4e775a0
Amrita7974 <amritakumari9343@gmail.com>
2026-04-20 22:49:53 +0530

    JS basic

---
+++ JSBasic/script.js
@@ -0,0 +1,16 @@
+console.log("This is webpage");
+console.log("this is page123");
+
+var a = 5;
+var b = 6;
+
+let x = 67;
+let y = 89;
+console.log(x==y);
+console.log(typeof x);
+
+console.log(a!=b);
+console.log(a+b);
+console.log(x/y);
+console.log(a+y);
+
