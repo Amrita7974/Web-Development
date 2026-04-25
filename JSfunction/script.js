8e6243ce2fb99a4d80e257707677c9305807c527
Amrita7974 <amritakumari9343@gmail.com>
2026-04-25 11:35:55 +0530

    function in JS

--- JSfunction/script.js
+++ JSfunction/script.js
@@ -46,5 +46,45 @@ function numbers(){
     else return false;
 }
 
+function sayHello() {
+  return "Hello World";
+}
+
+function sayHello() {
+  return "Hello World";
+}
+
+let message = sayHello();
+
+function multiply(a, b) {
+  return a * b;
+}
+
+
+// object method
+
+const obj = {
+  myFunction(x, y) {
+    return x * y;
+  },
+};
+
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+  return a * b;
+}
+
+const sum = add(2, 3); 
+
+function add(a, b) 
+{return a + b;}
+
+
+
+
+
+
+
 
 
