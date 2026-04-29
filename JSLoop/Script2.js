0dd128dc8348e3a8f79b1e2b8d38c7066625f54a
Amrita7974 <amritakumari9343@gmail.com>
2026-04-29 21:49:14 +0530

    Loop in   js

---
+++ JSLoop/Script2.js
@@ -0,0 +1,36 @@
+for(var i = 0; i<=10; i++){
+    console.log(i);
+    
+}
+
+for(let i=1; i<=21; i++){
+    if(i%2==0){
+
+         console.log(i);
+
+    }
+     
+}
+
+let a = [24,58,58,36,28,368];
+for(let i = 0; i<a.length; i++){
+    console.log(a[i]);
+}
+
+let b = [24,58,58,36,28,368];
+let i = 0;
+while(i<10){
+    console.log(b[i]);
+    i++;
+}
+
+let j = 1;
+while(j<=21){
+    if(j%2==0){
+    console.log(j);
+    }
+    j++;
+
+}
+
+
