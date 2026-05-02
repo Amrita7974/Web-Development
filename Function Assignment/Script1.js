e7fa4e8c81bd2952d222c33034ffa4d84ebab984
Amrita7974 <amritakumari9343@gmail.com>
2026-05-02 18:12:02 +0530

    JSFunction Assignment1

---
+++ Function Assignment/Script1.js
@@ -0,0 +1,59 @@
+//Create a simple function that prints "HelloWorld".
+function a() {
+  console.log("HelloWorld");
+}
+a();
+
+//Write a function that displays your name inthe console.
+function a1() {
+  console.log("Amrita");
+}
+a1();
+
+//Create a function that prints "Welcome toCollege".
+function a2() {
+  console.log("Welcome toCollege");
+}
+a2();
+
+//Write a function that takes a name as aparameter and prints "Hello [name]".
+function printname() {
+  console.log("Hello[name]");
+}
+printname("Amrita");
+
+//Create a function that takes two numbers andprints their sum.
+let x = 20;
+let y = 30;
+function sum() {
+  console.log(x + y);
+}
+sum();
+
+//Write a function that takes a number andprints its square.
+let b = 20;
+function square() {
+  console.log(b * b);
+}
+square();
+
+//Create a function that takes two numbers andreturns their addition.
+let l = 12;
+let k = 25;
+function add() {
+  console.log(l + k);
+}
+add();
+
+//Write a function that returns themultiplication of two numbers
+function multi(a, b) {
+  return a * b;
+}
+console.log(multi(5, 6));
+
+//.Create a function that takes a name and ageand prints a message
+function showdetails() {
+  console.log("name:" + name + "Age:" + age);
+}
+showdetails("Amrita", 19);
+
