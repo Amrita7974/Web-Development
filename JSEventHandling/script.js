9117787e908c4aba3028e994bd09fdbdda85af67
Amrita7974 <Amritakumari9343@gmail.com>
2026-05-09 10:38:37 +0530

    event handling in JS

---
+++ JSEventHandling/script.js
@@ -0,0 +1,33 @@
+function bulbOn(){
+    document.getElementById("bulb1").style.backgroundColor = "blue";
+}
+
+function bulbOff(){
+    document.getElementById("bulb1").style.backgroundColor = "white";
+}
+
+// function bulbred(){
+//     document.getElementById("bulb1").style.backgroundColor = "red";
+// }
+
+// function bulb2On(){
+//     document.getElementById("bulb1").style.backgroundColor = "green";
+// }
+
+// document.getElementById("bulb1color").addEventListener("change",changebulbcolor);
+
+// function changebulbColor(){
+//     const color = document.getElementById("bulb1color").value;
+//     document.getElementById("bulb1").style.backgroundColor = color;
+// }
+
+// function reset(){
+//     window.location.reload();
+    
+// }
+
+
+document.getElementById(bulbred).addEventListener("click", "bulbred");
+document.getElementById(bulb).addEventListener("click", "bulbred");
+document.getElementById(bulbred).addEventListener("click", "bulbred");
+
