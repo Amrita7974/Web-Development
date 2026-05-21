995bd70c9cf41d249edd2ff62d89e075278eb6a9
Amrita7974 <amritakumari9343@gmail.com>
2026-05-21 22:16:38 +0530

    Promises in JS

--- AdvanceJS/promise/Script.js
+++ AdvanceJS/promise/Script.js
@@ -10,3 +10,29 @@ getData().then((data) => {
     console.log(data);
 });
 
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("foo");
+  }, 300);
+});
+
+myPromise
+  .then(handleFulfilledA, handleRejectedA)
+  .then(handleFulfilledB, handleRejectedB)
+  .then(handleFulfilledC, handleRejectedC);
+
+  function add(num) {
+    return new Promise((resolve) => {
+        resolve(num + 50);
+    });
+}
+
+add(5)
+    .then((result) => {
+        console.log(result); 
+        return result * 3;
+    })
+    .then((result) => {
+        console.log(result); 
+    })
+
