543c58aec3eb6ada68e6e814620222f4938bd030
Amrita7974 <amritakumari9343@gmail.com>
2026-05-23 20:57:44 +0530

    Joke App and use of API

---
+++ projects/jokeApp/script.js
@@ -0,0 +1,12 @@
+async function getnewjoke() {
+
+    const API_URL = "https://official-joke-api.appspot.com/jokes/random";
+
+    const response = await fetch(API_URL);
+
+    const data = await response.json();
+
+    document.getElementById("setup").innerText = data.setup;
+
+    document.getElementById("delivery").innerText = data.punchline;
+}
