ca3c31c4954a5223f90149babd1085c063873727
Amrita7974 <amritakumari9343@gmail.com>
2026-06-02 22:32:47 +0530

    Functions in Js

---
+++ Function Assignment/Script.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 20;
+
+function sum(a, b) {
+  let z = a + b;
+  console.log(z);
+}
+sum(a, b);
+
+let lmn = function () {
+  console.log("Hello World");
+};
+console.log(lmn);
+
+const addarrow = (x,y) => x+y;
+console.log(addarrow(4,5));
