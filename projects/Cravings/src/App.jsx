d83f2042c55b5e6f27904cfee353c987e0c458ce
Amrita7974 <amritakumari9343@gmail.com>
2026-06-08 20:25:58 +0530

    add initial setup for React project with Vite and Tailwind CSS

---
+++ projects/Cravings/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return(
+    <>
+    <div className="bg-red-200 text-black p-3.25">App</div>
+    
+    <button className="bg-blue-500 px-5 py-2 mt-3 mx-5 rounded text-white hover:bg-blue-700 active:bg-amber-500">
+            submit
+          </button>
+   
+    </>
+  );
+};
+export default App; 
