3eb46b4ed38fce00109ab6e91b58a59be9bc4596
Amrita7974 <amritakumari9343@gmail.com>
2026-06-25 21:05:57 +0530

    feat: implement user model and update registration logic with photo handling

--- Backend/FirstBackend/index.js
+++ Backend/FirstBackend/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
 
+import express from "express";
 import AuthRouter from "./src/routers/auth.route.js";
 import PublicRouter from "./src/routers/public.route.js";
 import connectDB from "./src/config/dbConnection.config.js";
@@ -10,19 +10,20 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/auth",AuthRouter);
-app.use("/public",PublicRouter)
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
+
+//Default API
 app.get("/", (req, res) => {
   console.log("Default Get API Hit");
-  res.json({ message: "Welcome to my first backend project" });
+  res.json({ message: "Welcome to my first backend Project" });
 });
 
-// app.post("/login",(req,res) => {
-//     res.json({ message: "Login Sucessfull"});
-// });
-const port = process.env.port || 5000;
+
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log("Server Started on port:",port);
+  console.log("Server Started on port:", port);
   connectDB();
 });
--- Backend/FirstBackend/src/controllers/auth.controller.js
+++ Backend/FirstBackend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/usermodel.js";
+import User from "../models/user.model.js";
 export const RegisterUser = async(req, res) => {
   try{
     const{ fullName,email,phone,dob,gender,password} = req.body;
@@ -13,10 +13,30 @@ export const RegisterUser = async(req, res) => {
       return;
     }
 
+    const photoUrl = 'https:placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}';
 
-  } catch (error){ 
+const photo = {
+  url: photoUrl,
+  publicId: null,
+};
 
-  }
+const newUser = await User.create({
+  fullName,
+  email,
+  password,
+  phone,
+  gender,
+  dob,
+  photo,
+});
+
+res.status(201).json({ message: "User Created Successfully" });
+
+} catch (error) {
+  res.status(500).json({ message: "Internal Server Error" });
+}
+
+ 
 };
 
 export const LoginUser = (req, res) => {
